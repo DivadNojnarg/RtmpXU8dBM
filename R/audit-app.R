ab4e48a242823f32b0a5ba47457baf7900c51522
DivadNojnarg <dgranjon@ymail.com>
2022-05-10 10:13:08 +0200

    missing R functions

--- R/audit-app.R
+++ R/audit-app.R
@@ -1,3 +1,46 @@
+# Main shiny app
+shiny_bg <- function(path, port) {
+ # main app
+  options(shiny.port = port)
+  shiny::runApp(path)
+}
+
+# Start recorder
+recorder_bg <- function(port) {
+  shinyloadtest::record_session(
+    target_app_url = sprintf("http://127.0.0.1:%s", port),
+    host = "127.0.0.1",
+    port = 8600,
+    output_file = "recording.log",
+    open_browser = FALSE
+  )
+}
+
+start_r_bg <- function(fun, path = NULL, port = 3515) {
+ 
+  # remove NULL elements
+  args <- Filter(Negate(is.null), list(path = path, port = port))
+
+  process <- callr::r_bg(
+    func = fun,
+    args = args,
+    stderr= "",
+    stdout = ""
+  )
+
+  while (any(is.na(pingr::ping_port("127.0.0.1", 3515)))) {
+    message("Waiting for Shiny app to start...")
+    Sys.sleep(0.1)
+  }
+
+  attempt::stop_if_not(
+    process$is_alive(),
+    msg = "Unable to launch the subprocess"
+  )
+
+  process
+}
+
 record_loadtest <- function(path, timeout = 15, workers = 5) {
   message("\n---- BEGIN LOAD-TEST ---- \n")
   # start app + recorder
